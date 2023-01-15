e7c2d08578878c54d26454f73463f7cd137e8b12
HPvictor <122475612+HPvictor@users.noreply.github.com>
2023-01-15 20:12:36 -0300

    Delete Compilador directory

--- Compilador/binary.h
+++
@@ -1,7 +0,0 @@
-/****************************************************/
-/* Victor Hugo Teodoro Pimentel  RA 150976          */
-/* Compiladores - Prof. Galvão                      */                        
-/****************************************************/
-
-
-void generateBinary (AssemblyCode a, int size);
