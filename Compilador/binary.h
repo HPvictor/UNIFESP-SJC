c1f5c3847b4e5861f14e8c47852c71dbee6e9254
HPvictor <122475612+HPvictor@users.noreply.github.com>
2023-01-15 17:24:37 -0300

    Delete Compilador directory

--- Compilador/binary.h
+++
@@ -1,9 +0,0 @@
-/****************************************************/
-/* Ricardo Elizeu Neto  RA 104121                   */
-/* Lab de Compiladores - Prof. Galvão               */
-/* File: binary.h                                   */
-/* The code generator implementation                */
-/* for the C- compiler                              */
-/****************************************************/
-
-void generateBinary (AssemblyCode a, int size);
--- Compilador/script.sh
+++
@@ -1,4 +0,0 @@
-flex cminus.l
-bison -d cminus.y
-gcc -c *.c -fno-builtin-exp -Wno-implicit-function-declaration
-gcc *.o -lfl -o cminus -fno-builtin-exp
