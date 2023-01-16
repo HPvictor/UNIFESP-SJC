1f01e27313befdc4411263fac9dcdbbedc8a8df4
HPvictor <122475612+HPvictor@users.noreply.github.com>
2023-01-16 00:04:03 -0300

    Delete Compilador2 directory

--- Compilador2/AnalisadorSemanticoPaulo.4.1/binary.h
+++
@@ -1,6 +0,0 @@
-/****************************************************/
-/* The code generator implementation                */
-/* for the C- compiler                              */
-/****************************************************/
-
-void generateBinary (AssemblyCode a, int size);
--- Compilador2/AnalisadorSemanticoPaulo.4.1/script.sh
+++
@@ -1,4 +0,0 @@
-flex compiler.l
-bison -d compiler.y
-gcc -c *.c -fno-builtin-exp -Wno-implicit-function-declaration
-gcc *.o -lfl -o compiler -fno-builtin-exp
